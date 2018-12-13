5a97fc0727e3587606e1844af22a4b5700f56951
AdrienFerrer <adrien.ferrer@isep.fr>
2018-12-13 17:43:18 +0100

    finding + began select and list fill

--- src/Input.js
+++ src/Input.js
@@ -9,47 +9,69 @@ export class Input extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {song: ''};
+    this.state = {
+        song: '',
+        matching_songs:[],
+        selected_song:'',
+        selected_songs:[]
+    };
 
+    this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-    search (arr, term) {
-    var i, indices = [];
-    while (i < arr.length) {
-      if (arr[i] === term) {
-        indices.push(i);
-      }
-      i++;
-        }
-    return indices;
+
     }
 
-
+  handleClick(event){
+      console.log('hey');
+    this.setState({selected_song: event.target.value});
+    console.log(this.state.selected_song);
+    this.state.selected_songs.push(this.state.selected_song);
+  }  
 
 
   handleChange(event) {
     this.setState({song: event.target.value});
     this.state.song = event.target.value;
-    console.log(this.state.song);
+    console.log(event.target.value);
     //console.log(this.filter(allSongs,this.state.song));
 
-    console.log(this.search(allSongs,this.state.song));
+    this.state.matching_songs=[];
 
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.song);
-    event.preventDefault();
-    console.log(this.state.song);
+    for (var i=0;i<allSongs.length;i++){
+        if(this.state.song=='' || this.state.song==' ' ||  this.state.song=='  '){
+            break;
+        }
+        else{
+            if(allSongs[i].includes(this.state.song)==true){
+                //console.log(allSongs[i]);
+                this.state.matching_songs.push(allSongs[i]);
+            }
+        }
+    }
   }
 
   render() {
     return (
       <div>
         <form>
-            <input type="text" value={this.state.song} onChange={this.handleChange} placeholder="search a song"></input>
+            <input type="text" list="songs" value={this.state.song}  onChange={this.handleChange} placeholder="search a song"></input>
+                <datalist id="songs" onChange={this.handleClick}>
+                    {//IMPOSSIBLE DE METTRE un OnClick sur <option> !!!!
+                        this.state.matching_songs.map((song) =>
+                        <option key={song} value={song}>{song}</option>
+                    )}
+                </datalist>
+            <div>
+                <h4>Results :</h4>
+                <ul>
+                    {this.state.matching_songs.map((song) => <li key={song}>{song}</li>)}
+                </ul>
+
+                <h4>Selected Songs</h4>
+                    <ul>
+                        {this.state.selected_songs.map((song) => <li key={song}>{song}</li>)}
+                    </ul>
+            </div>
         </form>
       </div>
     )
